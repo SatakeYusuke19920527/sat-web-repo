334ce4c4ae13a5d1172311f780be07993290bdb7
SatakeYusuke19920527 <satkyusk@icloud.com>
2024-04-03 21:52:39 +0900

    next app first commit

--- sat-web-app-iac/module/swa.tf
+++ sat-web-app-iac/module/swa.tf
@@ -3,4 +3,7 @@ resource "azurerm_static_web_app" "swa" {
   name = var.swa_name
   resource_group_name = azurerm_resource_group.rg_app.name
   location = var.swa_location
+  tags = {
+    environment = "sat-web-app"
+  }
 }
